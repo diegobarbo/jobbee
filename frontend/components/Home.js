57fc7c9d8428dace838ba515309bc02de66446e1
Diego Barbo <diegobarbox@gmail.com>
2022-05-06 20:12:36 -0300

    render map with jobs coordinates

--- frontend/components/Home.js
+++ frontend/components/Home.js
@@ -5,7 +5,9 @@ import Link from "next/link";
 
 import JobItem from "./job/JobItem";
 
-const Home = () => {
+const Home = ({ data }) => {
+  const { jobs, count, resPerPage } = data;
+
   return (
     <>
       <div className="container container-fluid">
@@ -24,8 +26,7 @@ const Home = () => {
                 <Link href="/search">Go to Search</Link>
               </div>
             </div>
-            <JobItem />
-            <JobItem />
+            {jobs && jobs.map((job) => <JobItem key={job.id} job={job} />)}
           </div>
         </div>
       </div>
--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,6 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     API_URL: "http://localhost:8000",
+    MAPBOX_ACCESS_TOKEN:
+      "pk.eyJ1IjoiZGllZ29iYXJibyIsImEiOiJjbDJzeTdwYXcwM2RkM2tuMGJpempreWs2In0.E4qpuktdVxbvGrTaVgpE5g",
   },
 };
 
