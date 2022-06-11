56bd3a875f73a3557716fa16fed3b7c454252c5e
Diego Barbo <diegobarbox@gmail.com>
2022-06-10 21:18:21 -0300

    create login function

--- frontend/context/AuthContext.js
+++ frontend/context/AuthContext.js
@@ -1,12 +1,41 @@
+import axios from "axios";
 import { useState, useEffect, createContext } from "react";
+import { useRouter } from "next/router";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const [user, setUser] = useState(null);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [error, setError] = useState(null);
+
+  const router = useRouter();
+
+  //login user
+
+  const login = async ({ username, password }) => {
+    try {
+      setLoading(true);
+
+      const res = await axios.post("/api/auth/login", {
+        username,
+        password,
+      });
+
+      if (res.data.success) {
+        setIsAuthenticated(true);
+        setLoading(false);
+        router.push("/");
+      }
+    } catch (error) {
+      setLoading(false);
+      setError(
+        error.response &&
+          (error.response.data.detail || error.response.data.error)
+      );
+    }
+  };
 
   return (
     <AuthContext.Provider
@@ -15,9 +44,12 @@ export const AuthProvider = ({ children }) => {
         user,
         error,
         isAuthenticated,
+        login,
       }}
     >
       {children}
     </AuthContext.Provider>
   );
 };
+
+export default AuthContext;
