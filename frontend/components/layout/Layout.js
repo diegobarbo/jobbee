e162dfc13553dac51b6824c13248ded47048f2f1
Diego Barbo <diegobarbox@gmail.com>
2022-06-13 15:06:12 -0300

    react toastify to display errors

--- frontend/components/layout/Layout.js
+++ frontend/components/layout/Layout.js
@@ -5,6 +5,9 @@ import Script from "next/script";
 import Header from "./Header";
 import Footer from "./Footer";
 
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 const Layout = ({ children, title = "Find your job now!" }) => {
   return (
     <div>
@@ -30,6 +33,8 @@ const Layout = ({ children, title = "Find your job now!" }) => {
         strategy="beforeInteractive"
         src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js"
       ></Script>
+
+      <ToastContainer position="bottom-right" />
 
       <Header />
       {children}
